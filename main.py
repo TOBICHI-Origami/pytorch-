b29b5bbc36dbd76bf6d097a75c238d95a09ca008
TOBICHI-Origami <93001743+TOBICHI-Origami@users.noreply.github.com>
2022-02-20 11:49:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 #%% [code]
 # This is a sample Python script.
 
@@ -13,7 +12,7 @@ import torch.nn.functional as F
 from torchtext.data import get_tokenizer, to_map_style_dataset
 from torchtext.datasets import IMDB
 from torchtext.vocab import build_vocab_from_iterator
-
+device=torch.device("cuda")
 VOCAB_SIZE = 15000
 class GCNN(nn.Module):
     def __init__(self,vocab_size=VOCAB_SIZE,embedding_dim = 64, num_class =2):
@@ -52,7 +51,7 @@ def yield_token(train_data_iter, tokenizer):
     for i , sample in enumerate(train_data_iter):
         label , comment = sample
         yield tokenizer(comment)
-train_data_iter = IMDB(root='./deeeeeep',split='train')
+train_data_iter = IMDB(root='.deeeeeep',split='train')
 tokenizer = get_tokenizer("basic_english")
 vocab = build_vocab_from_iterator(yield_token(train_data_iter,tokenizer),min_freq=20,specials=['<unk>'])
 vocab.set_default_index(0)
@@ -82,7 +81,8 @@ def train(train_data_loader , eval_data_loader, model ,optimizer ,num_epoch,log_
     if resume!=' ':
         print("logging from {}".format(resume))
         checkpoint = torch.load(resume)
-        model.load_state_dict(checkpoint['optimzer_state_dict'])
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         start_epoch = checkpoint['epoch']
         start_step = checkpoint['step']
 
@@ -92,9 +92,11 @@ def train(train_data_loader , eval_data_loader, model ,optimizer ,num_epoch,log_
 
         for batch_index,(target,token_index) in enumerate(train_data_loader):
             optimizer.zero_grad()
+            target = target.to(device)
+            token_index=token_index.to(device)
             step = num_batches*(epoch_index)+batch_index+1
             logits = model(token_index)
-            bce_loss = F.binary_cross_entropy(torch.sigmoid(logits),F.one_hot(target,num_classes=2).to(torch.float32))
+            bce_loss = F.binary_cross_entropy(torch.sigmoid(logits),F.one_hot(target,num_classes=2).to(torch.float32)).to(device)
             ema_loss=0.9*ema_loss+0.1*bce_loss
             bce_loss.backward()
             nn.utils.clip_grad_norm_(model.parameters(),0.1)
@@ -107,7 +109,8 @@ def train(train_data_loader , eval_data_loader, model ,optimizer ,num_epoch,log_
                 torch.save({
                     'epoch':epoch_index,
                     'step':step,
-                    'model_state_dict': optimizer.state_dict(),
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict':optimizer.state_dict(),
                     'loss':bce_loss,
                 },save_file)
                 print("checkpoint has been saved in {}".format(save_file))
@@ -118,28 +121,56 @@ def train(train_data_loader , eval_data_loader, model ,optimizer ,num_epoch,log_
                 total_acc_acount =0
                 total_account = 0
                 for eval_batch_index,(eval_target,eval_token_index) in enumerate(eval_data_loader):
+                    eval_target = eval_target.to(device)
+                    eval_token_index = eval_token_index.to(device)
+                    total_account+=eval_target.shape[0]
                     eval_logits = model(eval_token_index)
                     total_acc_acount +=(torch.argmax(eval_logits,dim=-1)==eval_target).sum().item()
-                    eval_bce_loss = F.binary_cross_entropy(torch.sigmoid(eval_logits),F.one_hot(eval_target,num_classes=2).to(torch.float32))
+                   
+                    eval_bce_loss = F.binary_cross_entropy(torch.sigmoid(eval_logits),F.one_hot(eval_target,num_classes=2).to(torch.float32)).to(device)
                     ema_eval_loss = 0.9*ema_eval_loss+0.1*eval_bce_loss
-                print("accuracy percentage is {}".format(total_acc_acount/(8*len(eval_data_loader))))
+                print("accuracy percentage is {}".format(total_acc_acount/(total_account)))
                 model.train()
-
+def ceshi(a,resume):
+        print("logging from {}".format(resume))
+        checkpoint = torch.load(resume)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        start_epoch = checkpoint['epoch']
+        start_step = checkpoint['step']
+        target = [0]
+        target = torch.tensor(target).to(torch.int64).to(device)
+        token_index = []
+        max_len=256
+        tokens = tokenizer(a)
+        token_index.append(vocab(tokens))
+        token_index = [index+[0]*(max_len-len(index))for index in token_index]
+        b=(torch.tensor(token_index).to(torch.int32))
+        b=b.to(device)
+        eval_logits = model(b)
+        print(eval_logits)
+        return "positive" if(torch.argmax(eval_logits,dim=-1)==target) else "negative"
 
 #测试代码
 if __name__ == '__main__':
     model =GCNN()
+    model.to(device)
     print("模型总参数：",sum(p.numel() for p in model.parameters()))
-    optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 
-    train_data_iter = IMDB(root='.deeeeep',split = 'train')
+    train_data_iter = IMDB(root='.deeeeeep',split = 'train')
     train_data_loader = torch.utils.data.DataLoader(to_map_style_dataset(train_data_iter),batch_size=BATCH_SIZE,collate_fn = collate_fn,shuffle=True)
 
-    eval_data_iter = IMDB(root="./deeeeeep",split='test')
+    eval_data_iter = IMDB(root=".deeeeeep",split='test')
     eval_data_loader = torch.utils.data.DataLoader(to_map_style_dataset(eval_data_iter),batch_size=8,collate_fn=collate_fn)
     resume = ' '
 
-    train(train_data_loader,eval_data_loader,model,optimizer, num_epoch=10 ,log_step_interval=20,save_step_interval=500,eval_step_interval=100,save_path='./logs_imdb_text_classification',resume=resume)
-
-
-
+    train(train_data_loader,eval_data_loader,model,optimizer, num_epoch=10 ,log_step_interval=20,save_step_interval=500,eval_step_interval=300,save_path='./logs_imdb_text_classification',resume=resume)
+    
+    #测试
+    resume = './logs_imdb_text_classification/step3500.pt'
+    a=("i think it is very bad")
+    b=("it look like a beautiful boy i think it is good")
+    
+    print(ceshi(a,resume))
+    print(ceshi(b,resume))
